ac768f50e58acc8b1ac596ed699ab46cf13f5a66
Shinhee Park <52345499+bobsini601@users.noreply.github.com>
2022-08-06 21:29:06 +0900

    [BOJ] 팩토리얼/브론즈5/1시간이내

--- shinhee/BOJ/Main_10872_팩토리얼.java
+++ shinhee/BOJ/Main_10872_팩토리얼.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Main_10872_팩토리얼 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		// 초기값 1 설정 
 		int result=1;
+		// 입력값 받기
 		int n = sc.nextInt();
 		for(int i=1;i<=n;i++) {
 			result*=i;
